14505e98a78f85def18e04444c08fd932ee62351
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-17 14:37:54 +0500

    update.

--- src/app/api/todos/route.js
+++ src/app/api/todos/route.js
@@ -1,5 +1,34 @@
 const todos = [
-    "Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6", "Task 7",
+    {
+        id: 1,
+        todo: "Task 1",
+        isCompleted: false
+    },
+    {
+        id: 2,
+        todo: "Task 2",
+        isCompleted: false
+    },
+    {
+        id: 3,
+        todo: "Task 3",
+        isCompleted: false
+    },
+    {
+        id: 4,
+        todo: "Task 4",
+        isCompleted: false
+    },
+    {
+        id: 5,
+        todo: "Task 5",
+        isCompleted: false
+    },
+    {
+        id: 6,
+        todo: "Task 6",
+        isCompleted: false
+    },
 ]
 
 export async function GET(request) {
