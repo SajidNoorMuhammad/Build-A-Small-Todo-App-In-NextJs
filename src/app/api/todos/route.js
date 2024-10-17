6f67e4708d028b47c22014402fd501cce6547dde
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-17 14:19:28 +0500

    update.

---
+++ src/app/api/todos/route.js
@@ -0,0 +1,10 @@
+const todos = [
+    "Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6", "Task 7",
+]
+
+export async function GET(request) {
+    return Response.json({
+        data: todos,
+        msg: "Todos Fetched Successfully"
+    })
+}
