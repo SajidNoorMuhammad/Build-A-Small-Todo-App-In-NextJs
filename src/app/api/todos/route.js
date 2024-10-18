21a3151e52805dff3f4a25625cd0267e89cdaea6
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-18 11:20:48 +0500

    update.

--- src/app/api/todos/route.js
+++ src/app/api/todos/route.js
@@ -37,3 +37,10 @@ export async function GET(request) {
         msg: "Todos Fetched Successfully"
     })
 }
+
+export async function POST(request) {
+    const data = await request.json();
+    console.log(data);
+
+    return Response
+}
