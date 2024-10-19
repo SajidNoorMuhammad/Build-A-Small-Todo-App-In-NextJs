1cfacc6c5d9a6dd96eda899af5522fd9501f4c16
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-19 14:20:06 +0500

    update api link.

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 
 export async function addTodos(formData) {
     const todo = formData.get('todo');
-    fetch('http://localhost:3000/api/todos',
+    fetch('https://build-a-small-todo-app-in-next-js.vercel.app/api/todos',
         {
             method: "Post",
             body: JSON.stringify({ todo })
